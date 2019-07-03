3b96b11e09984a01b1cbcc26af16d34803a54747
Paulo Matos <pmatos@igalia.com>
2019-07-03 13:49:17 +0200

    Improve debug information for analyze-build script

--- scan-build-webkit.sh
+++ scan-build-webkit.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+CWD=$PWD
 INSTALL_DIR=$PWD/install
 mkdir $INSTALL_DIR
 
@@ -17,23 +18,23 @@ wget https://github.com/Z3Prover/z3/releases/download/Z3-4.8.5/z3-4.8.5-x64-debi
 unzip z3-4.8.5-x64-debian-8.11.zip
 mv z3-4.8.5-x64-debian-8.11/bin z3-4.8.5-x64-debian-8.11/include $INSTALL_DIR
 
+export PATH=$INSTALL_DIR/bin:$PATH
 git clone --depth=1 https://github.com/llvm/llvm-project.git
 cd llvm-project
 wget -O bug41809.patch https://bugs.llvm.org/attachment.cgi?id=22160
 patch -p1 < bug41809.patch
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCLANG_ANALYZER_ENABLE_Z3_SOLVER=ON -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS=clang -DZ3_INCLUDE_DIR=$INSTALL_DIR/include/ -DCMAKE_BUILD_TYPE=Release ../llvm/
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DLLVM_ENABLE_Z3_SOLVER=ON -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_PROJECTS=clang -DZ3_INCLUDE_DIR=$INSTALL_DIR/include/ -DCMAKE_BUILD_TYPE=Release ../llvm/
 ninja
 ninja install
 
+cd $CWD
 pip install scan-build
 git clone --depth=1 git://git.webkit.org/WebKit.git
 cd WebKit
 Tools/Scripts/build-jsc --jsc-only --release
 cd WebKitBuild/Release
-find . -name '*.o' -exec rm \{\} \;
-rm -Rf bin/ lib/
-analyze-build --cdb compile_commands.json -o build-analysis -analyzer-config 'crosscheck-with-z3=true'
+analyze-build -v -v -v -v --cdb compile_commands.json -o build-analysis -analyzer-config 'crosscheck-with-z3=true'
 
 mv build-analysis /artifacts
