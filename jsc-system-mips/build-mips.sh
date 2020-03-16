c350b6196c6728fdf48b3e0a069441ca0e63e592
Paulo Matos <pmatos@linki.tools>
2020-03-16 13:04:28 +0100

    Create qcow2 image when building toolchain to use as backing image

--- jsc-system-mips/build-mips.sh
+++ jsc-system-mips/build-mips.sh
@@ -20,6 +20,13 @@ popd
 pushd ${DEST}
 make O=$PWD -C ${TMP_PATH}/buildroot BR2_EXTERNAL=${TMP_PATH}/jsc-br2-external qemu-mips32elr2-jsc_defconfig 2>&1 | tee configure.log
 make BR2_JLEVEL=16 2>&1 | tee build.log
+
+# Need to convert image to use it as backing file
+echo "Converting raw image to qcow2"
+if ! host/bin/qemu-img -O qcow2 images/rootfs.ext2 images/rootfs.qcow2; then
+    echo "Failed to convert image"
+    exit 1
+fi
 popd
 
 echo "Cleaning up temporary folder ${TMP_PATH}"
