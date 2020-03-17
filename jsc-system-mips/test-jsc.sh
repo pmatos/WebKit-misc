83ff7c480c35fa3cd462b4917a6ff0f3c84a2750
Paulo Matos <pmatos@linki.tools>
2020-03-17 10:25:16 +0100

    Improvements to test-jsc including new --filter and debug/release modes

--- jsc-system-mips/test-jsc.sh
+++ jsc-system-mips/test-jsc.sh
@@ -1,15 +1,115 @@
 #! /bin/bash
+# Runs JSC tests on MIPS using an external buildroot toolchain
+#
+# Usage:
+#      test-jsc.sh [ --? | -h | --help ]
+#                  [ --version]
+#                  [ --release | --debug ]
+#                  [ --filter "..." ]
+#                  [ --vms "..." ]
+#                  [ --port "..." ]
+#                  webkit-directory
+#                  buildroot-path
+#
 
-# Receives 4 arguments
-# N - number of workers to use
-# P - starting port to use , ports [P, P+N] should be free
-# WebKitPath - Path to WebKit directory
-# MIPSBuildrootPath - Path to MIPS Buildroot
+PROGRAM=$(basnemae "$0")
+VERSION=1.0
 
-N=$1
-P=$2
-WEBKIT_PATH=$3
-BRPATH=$4
+DATESTART=$(date +%s%N)
+
+source "./common.sh"
+
+usage()
+{
+    cat <<EOF
+Usage:
+	$PROGRAM 
+		 [ -h | --help | --? ]     Show help and exit
+		 [ --version ]             Show version and exit
+		 [ --release | --debug ]   JSC test mode (default: release)
+                 [ --vms N ]               Number of mips vms to start for testing
+		 [ --filter REGEX ]        Filter for tests, passed unmodified to run-javascriptcore-tests
+		 [ --port P ]              Starting port for VMs (ports [P, P+N-1] need to be free
+		 webkit-directory          Directory with WebKit checkout
+		 buildroot-directory       Directory with Buildroot install
+EOF
+}
+
+
+N=
+P=
+DEBUG=0
+FILTER=
+
+while test $# -gt 0
+do
+    case $1 in
+	--version )
+	    version
+	    exit 0
+	    ;;
+	--help | -h | '--?' )
+	    usage_and_exit 0
+	    ;;
+	--release )
+	    DEBUG=0
+	    break
+	    ;;
+	--debug )
+	    DEBUG=1
+	    break
+	    ;;
+	--vms )
+	    shift
+	    N=$1
+	    break
+	    ;;
+	--port )
+	    shift
+	    P=$1
+	    break
+	    ;;
+	--filter )
+	    shift
+	    FILTER=$1
+	    break
+	    ;;
+	-*)
+	    error "Unrecognized option: $1"
+	    ;;
+	*)
+	    break
+	    ;;
+    esac
+    shift
+done
+
+# Argument and flag option checks
+
+if [ -z "${P}" ]; then
+    error "need --port"
+fi
+
+if [ -z "${N}" ]; then
+    error "need --vms"
+fi
+
+if [ "$#" != "2" ]; then
+    error "expected two arguments, got $#"
+fi
+
+WEBKIT_PATH=$1
+BRPATH=$2
+
+DFLAG="--release"
+if [[ ${DEBUG} == "1" ]]; then
+    DFLAG="--debug"
+fi
+
+FFLAG=""
+if [[ -n "${FILTER}" ]]; then
+    FFLAG="--filter ${FILTER}"
+fi
 
 # Tests a JSC installation using qemu-system
 #
@@ -23,13 +123,13 @@ BRPATH=$4
 # 3. Run run-javascriptcore-tests passing in the remotes file
 # 4. Kill each of the qemu-systems and remove the HDDs
 
-TESTTMP_PATH=$(mktemp -d)
+TESTTMP_PATH="$(mktemp -d)"
 
-QEMUIMG_PATH=$BRPATH/host/bin/qemu-img
-QEMU_PATH=$BRPATH/host/bin/qemu-system-mipsel
-HDD_PATH=$BRPATH/images/rootfs.qcow2
-KERNEL_PATH=$BRPATH/images/vmlinux
-REMOTES_PATH=$(mktemp)
+QEMUIMG_PATH="${BRPATH}/host/bin/qemu-img"
+QEMU_PATH="${BRPATH}/host/bin/qemu-system-mipsel"
+HDD_PATH="${BRPATH}/images/rootfs.qcow2"
+KERNEL_PATH="${BRPATH}/images/vmlinux"
+REMOTES_PATH="$(mktemp)"
 
 declare -a IMAGES
 declare -a PORTS
@@ -37,26 +137,26 @@ declare -a PIDS
 
 # Assign image paths for each worker
 setup_images() {
-    for i in `seq 1 $N`
+    for i in $(seq 1 "${N}")
     do
-	local p=${TESTTMP_PATH}/rootfs-${i}.qcow2
-	${QEMUIMG_PATH} create -q -b $HDD_PATH $p
+	local p
+	p="${TESTTMP_PATH}/rootfs-${i}.qcow2"
+	"${QEMUIMG_PATH}" create -q -b "${HDD_PATH}" "${p}"
 	IMAGES[${i}]=$p
-	echo "Creating image for machine ${i} at ${p}"
+	progress "Creating image for machine ${i} at ${p}"
     done
 }
 
 # Assign free port numbers for each worker
 setup_ports() {
-    for i in `seq 1 $N`
+    for i in $(seq 1 "${N}")
     do
-	local port=$(( $P + $i ))
-	if $(lsof -i -P -n | grep LISTEN | grep -q ":${port}")
-	then
-	    echo "Port ${port} is not free to use"
-	    exit 1
+	local port
+	port=$(( P + i ))
+	if lsof -i -P -n | grep LISTEN | grep -q ":${port}"; then
+	    error "Port ${port} is not free to use"
 	fi
-	PORTS[${i}]=${port}
+	PORTS[${i}]="${port}"
     done
 }
 
@@ -64,73 +164,77 @@ setup_images
 setup_ports
 
 # Start qemu-system and store pids in PIDS array
-for i in `seq 1 $N`
+for i in $(seq 1 "${N}")
 do
     $QEMU_PATH -M malta \
 	       -m 2G \
-	       -kernel $KERNEL_PATH \
+	       -kernel "${KERNEL_PATH}" \
 	       -append "nokaslr root=/dev/hda" \
 	       -nographic \
 	       -net nic \
 	       -net user,hostfwd=tcp::${PORTS[${i}]}-:22 \
 	       -serial none \
 	       -monitor none \
-	       -drive format=raw,file=${IMAGES[${i}]} &
+	       -drive format=raw,file="${IMAGES[${i}]}" &
     PIDS[${i}]=$!
-    echo "Starting virtual mips machine with pid ${PIDS[${i}]} listening to ssh on port ${PORTS[${i}]}"
+    progress "Starting virtual mips machine with pid ${PIDS[${i}]} listening to ssh on port ${PORTS[${i}]}"
 done
 
 # Wait until we can communicate with the machines
-echo "Waiting for machines to boot..."
+progress "Waiting for machines to boot..."
 sleep 5
-for i in `seq 1 $N`
-do
-    echo -n "Trying to reach machine ${i}: "
-    pid=${PIDS[${i}]}
+for i in $(seq 1 "$N"); do
+    progress "Trying to reach machine ${i}"
+    port=${PORTS[${i}]}
     retries=3
     
-    while [[ $retries > 0 ]]
+    while [[ $retries -gt 0 ]]
     do
-	if ! ssh -p ${PORTS[${i}]} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no jsc@localhost true; then
-	    echo -n "FAILED "
+	if ! ssh -p "$port" -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no jsc@localhost true; then
+	    progress "FAILED ${i}"
 	    retries=$(( retries - 1 ))
 	    sleep 20
 	else
-	    echo "SUCCESS"
+	    progress "SUCCESS ${i}"
 	    break
 	fi
     done
 
     if [[ $retries == 0 ]]; then
-	echo "can't reach machine $i"
-	exit 1
+	error "can't reach machine $i"
     fi
 done
 
 # Create json file with information about remotes
-echo "Creating remotes file $REMOTES_PATH"
-echo '{"remotes": ['                             >> $REMOTES_PATH
-for i in `seq 1 $N`; do
-    echo -n '{"name": "virtual'                  >> $REMOTES_PATH
-    echo -n ${i}                                 >> $REMOTES_PATH
-    echo -n '", "address": "jsc@localhost:'      >> $REMOTES_PATH
-    echo -n ${PORTS[${i}]}                       >> $REMOTES_PATH
-    echo -n '", "remoteDirectory": "/home/jsc"}' >> $REMOTES_PATH
-    if [[ $i != $N ]]; then
-	echo ','                                 >> $REMOTES_PATH
-    fi
+progress "Creating remotes file $REMOTES_PATH"
+echo '{"remotes": ['                             >> "$REMOTES_PATH"
+for i in $(seq 1 "$N"); do
+    {
+	echo -n '{"name": "virtual'
+	echo -n "${i}"
+	echo -n '", "address": "jsc@localhost:'
+	echo -n ${PORTS[${i}]}
+	echo -n '", "remoteDirectory": "/home/jsc"}'
+	if [[ "$i" != "$N" ]]; then
+	    echo ','
+	fi
+    } >> "$REMOTES_PATH"
 done
-echo '] }'                                       >> $REMOTES_PATH
+echo '] }'                                       >> "$REMOTES_PATH"
+
+progress "running tests with output redirected to stdout"
 
 # Run tests through run-javascriptcore-tests
-${WEBKIT_PATH}/Tools/Scripts/run-javascriptcore-tests --no-build --no-fail-fast --release --memory-limited --remote-config-file $REMOTES_PATH --no-testmasm --no-testair --no-testb3 --no-testdfg --no-testapi --jsc-only 2>&1 | tee $TESTTMP_PATH/testing.log
+"${WEBKIT_PATH}"/Tools/Scripts/run-javascriptcore-tests --no-build --no-fail-fast "${DFLAG}" --memory-limited --remote-config-file "${REMOTES_PATH}" --no-testmasm --no-testair --no-testb3 --no-testdfg --no-testapi --jsc-only "${FFLAG}" 2>&1
 
 # Killall qemu systems and clean up HDDs
-for i in `seq 1 $N`; do
-    echo "Cleaning up machine ${i}"
+for i in $(seq 1 "${N}"); do
+    progress "Cleaning up machine ${i}"
     kill ${PIDS[${i}]}
 done
-echo "Removing temporary remotes file ${REMOTES_PATH}"
-rm ${REMOTES_PATH}
-echo "Removing temporary path ${TESTTMP_PATH}"
-rm -R ${TESTTMP_PATH}
+progress "Removing temporary remotes file ${REMOTES_PATH}"
+rm "${REMOTES_PATH}"
+progress "Removing temporary path ${TESTTMP_PATH}"
+rm -R "${TESTTMP_PATH}"
+
+progress "Testing finished successfully"
